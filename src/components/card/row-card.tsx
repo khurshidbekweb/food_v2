9eb63a1aeb4eb359cdc8a10242d237706462491d
karimoffabdulatif <akarimoff001@gmail.com>
2025-01-18 20:00:24 +0500

    change_stile

--- src/components/card/row-card.tsx
+++ src/components/card/row-card.tsx
@@ -1,33 +1,68 @@
-// import { Button } from "../ui/button";
-
+import { Button } from "../ui/button";
 import { IMG_BASE_URL } from "@/constants";
 import { useStore } from "@/store";
 import { Food } from "@/types";
+import { useState } from "react";
 
-interface PropsFood{
-    food:Food
+interface PropsFood {
+  food: Food;
 }
 
-const RowCard = ({food}:PropsFood) => {
-    const {language} = useStore()
-    console.log(food);
-    
-    return (
-        <div className="flex items-start space-x-4 p-1 border rounded-lg shadow-sm">
-            <img
-                src={`${IMG_BASE_URL}${food.image}`}
-                alt={'food img'}
-                className="rounded-md object-cover w-[180px] h-[120px]"
-            />
-            <div className="flex-1 flex flex-col justify-start items-start">
-                    <h3 className="font-semibold text-lg">{food.name[language?.code]}</h3>
-                    <p className="text-gray-600 text-[18px] font-medium mt-5">{food.price} so'm</p>
-                {/* <Button variant="outline" className="bg-[#E8FAF9] text-[#1BC5BD] h-[30px] py-2 px-7 rounded-full">
-                    +Qo'shish
-                </Button> */}
-            </div>            
+const RowCard = ({ food }: PropsFood) => {
+  const { language } = useStore();
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="">
+      <img
+        src={`${IMG_BASE_URL}${food.image}`}
+        alt="food img"
+        className="rounded-lg object-cover w-full h-[300px]"
+      />
+
+      <div className="mt-2">
+        <h3 className="font-semibold text-xl text-gray-800">
+          {food.name[language?.code]}
+        </h3>
+        <div className="flex justify-between">
+          <div>
+            <p className="text-gray-600 text-3xl font-medium mt-2">
+              {food.price.toLocaleString()} so'm
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            {count > 0 && (
+              <Button
+                variant="outline"
+                className="bg-[#8833EE] text-white text-3xl rounded-full w-12 h-12 pb-3 flex items-center justify-center border-[#8833EE]"
+                onClick={handleDecrease}
+              >
+                -
+              </Button>
+            )}
+            {count > 0 && <span className="text-xl">{count}</span>}
+            <Button
+              variant="outline"
+              className="bg-[#8833EE] text-white text-3xl rounded-full w-12 h-12 pb-3 flex items-center justify-center border-[#8833EE] leading-none"
+              onClick={handleIncrease}
+            >
+              +
+            </Button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RowCard;
