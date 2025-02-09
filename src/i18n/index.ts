7f04f3454514f3c33264a43de222b0c82a6f9356
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-02-10 00:53:57 +0500

    add setTimoute

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
-
 import translationUZ from "@/locales/uz/translation.json";
 import translationRU from "@/locales/ru/translation.json";
 import translationEN from "@/locales/en/translation.json";
 import translationTR from "@/locales/tr/translation.json";
+
+if(!localStorage.getItem('restaurant')!){
+    setTimeout(() => {
+      window.location.reload()
+    }, 1000)
+}
+
 
 i18n.use(initReactI18next).init({
   resources: {
